f189aeda4e950b9feaa2362571ab27baabfd147a
Guntur W <ggm5850@gmail.com>
2025-07-15 17:47:58 +0700

    feat: create log feature

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
+const fs = require("fs");
 const qrcode = require("qrcode-terminal");
 
 // Inisialisasi client dengan LocalAuth agar sesi tersimpan
@@ -17,26 +18,74 @@ client.on("ready", () => {
 });
 
 // Balasan jika pesan sesuai
-client.on("message", (message) => {
+client.on("message", async (message) => {
   console.log(message.body);
-  if (
-    !message.from.includes("@g.us") &&
-    message.body.toLowerCase() == "halo kak, saya ingin order skripsi."
-  ) {
-    message.reply(`Halo 👋
+  const contact = await message.getContact();
+  const nama = contact.pushname || "{Undefined}";
+  const nomor = contact.number;
+  const pesan = message.body;
 
-Terima kasih sudah menghubungi kami terkait order skripsi.
-Silakan kirim detail berikut agar kami dapat memproses:
-Nama:
-Judul skripsi:
-Jumlah halaman:
-Deadline:
+  // Membuat timestamp format DD-MM-YYYY HH:MM:SS
+  const now = new Date();
+  const timestamp = now
+    .toLocaleString("id-ID", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    })
+    .replace(/\./g, ":");
 
-Kami akan segera memproses permintaan Anda 🙏`);
+  let log = ``;
+  if (message.from.includes("@g.us")) {
+    // Jika pesan dari grup
+    const chat = await message.getChat();
+    const namaGrup = chat.name;
+    log = `[${timestamp}] [${namaGrup}] (${nama}:${nomor}) : ${pesan}\n`;
+  } else {
+    // Jika pesan personal
+    log = `[${timestamp}] (${nama}:${nomor}) : ${pesan}\n`;
   }
+  fs.appendFileSync("sent_messages.log", log);
+
   if (
     !message.from.includes("@g.us") &&
-    message.body.toLowerCase() == "viki poke"
+    message.body.toLowerCase().startsWith("halo kak, saya ingin order skripsi")
+  ) {
+    message.reply(
+      `Halo 👋
+
+        Terima kasih sudah menghubungi kami terkait order skripsi.
+        Silakan kirim detail berikut agar kami dapat memproses:
+        Nama:
+        Judul skripsi:
+        Jumlah halaman:
+        Deadline:
+
+        Kami akan segera memproses permintaan Anda 🙏`
+    );
+  } else if (
+    !message.from.includes("@g.us") &&
+    message.body.toLowerCase().startsWith("halo kak, saya ingin order website")
+  ) {
+    message.reply(
+      `Halo 👋
+
+        Terima kasih sudah menghubungi kami terkait order skripsi.
+        Silakan kirim detail berikut agar kami dapat memproses:
+        Nama:
+        Judul skripsi:
+        Jumlah halaman:
+        Deadline:
+
+        Kami akan segera memproses permintaan Anda 🙏`
+    );
+  } else if (
+    !message.from.includes("@g.us") &&
+    message.body.toLowerCase().startsWith("viki poke")
   ) {
     message.reply(`Emang`);
   }
